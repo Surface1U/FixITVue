06f0091407628e0f5e187ce802b1bee4d95c407e
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-05-06 20:56:26 +0300

    Add files via upload

---
+++ SecondView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="student in students" :key="student.code">
+        {{ student.fullName }} ({{ student.code }})
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+  <script>
+
+// import { mapActions } from 'vuex';
+// import { mapGetters } from 'vuex';
+
+export default {
+  name: 'What the',
+
+  computed: {
+    students() {
+      return Array.from(this.$store.state.student.students.values())
+    }
+
+  }
+}
+
+
+  </script>
