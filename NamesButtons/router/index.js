b347dc76ae6935191d69fe05b50e63f56f02d1a3
[Surface!!u] <60102276+Surface1U@users.noreply.github.com>
2023-05-08 16:21:06 +0300

    Create index.js

---
+++ NamesButtons/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Grades from '@/views/Grades.vue'
+import StudentDetail from '@/components/grade/StudentDetail.vue';
+
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'Grades',
+        component: Grades
+    },
+    {
+        path: '/second',
+        name: 'SecondView',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "second" */ '@/views/SecondView.vue')
+    },
+    {
+        path: '/student/:code',
+        name: 'student-detail',
+        component: StudentDetail,
+        props: true
+      }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
